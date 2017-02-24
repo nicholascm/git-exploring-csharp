bd32a316db4f2e198284d7fcb49bcf67a1c5c26f
nicholascm90 <nicholascm90@gmail.com>
2017-02-24 06:39:07 -0500

    added restsharp for core

--- src/ConsolePracticeApp/GitService.cs
+++ src/ConsolePracticeApp/GitService.cs
@@ -1,25 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace ConsolePracticeApp
 {
     public class GitService
     {
-        public const string url = "https://api.github.com/search/repositories?q=";  
+        private const string url = "https://api.github.com/search/repositories?q=";  
 
         public string username; 
 
-        public GitService(string username)
+        public GitService(string username, string url)
         {
             this.username = username; 
         }
 
         public void getRepositories()
         {
-            Console.WriteLine("repositories gotten"); 
-            
+            Console.WriteLine("repositories gotten");
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponseAsync();
+
         }
     }
 }
